8c27f315c48f67f2b302a1af2055554cae21cbbb
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2019-07-22 15:12:22 +0200

    Add in some more queries

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,31 @@
 import * as express from 'express'
 import { buildSchema } from 'graphql'
-const graphqlHTTP = require('express-graphql');
+const graphqlHTTP = require('express-graphql')
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
   type Query {
-    hello: String
+    hello: String,
+    random: Float!,
+    rollDice(numDice: Int!, numSides: Int): [Int]
   }
 `);
 
 // The root provides a resolver function for each API endpoint
 const root = {
-    hello: () => {
-        return 'Hello world!';
+    hello() {
+        return 'Hello world!'
     },
+    random() {
+        return Math.random()
+    },
+    rollDice({numDice, numSides}) {
+        let rolls = []
+        while (numDice--) {
+            rolls.push(1 + Math.floor(Math.random() * numSides))
+        }
+        return rolls
+    }
 };
 
 const app = express();
